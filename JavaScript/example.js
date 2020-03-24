ebc047aa6d0a1820d20efe14ca16b3ca8fd66a04
LucasD <53497196+MisterPCLucas@users.noreply.github.com>
2020-03-25 00:30:22 +0100

    ES6, var depreciated
    changed var to const, for ES6 in line 2
    For a good example for beginners in web development

--- JavaScript/example.js
+++ JavaScript/example.js
@@ -1,5 +1,5 @@
 // For use with Node.js
-var ms = require('./minestat');
+const ms = require('./minestat');
 ms.init('minecraft.frag.land', 25565, function(result)
 {
   console.log("Minecraft server status of " + ms.address + " on port " + ms.port + ":");
